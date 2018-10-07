5b32dedcfbdc4120d59505a1a52809b9980492cb
madaskalas <marios.daskalas@gmail.com>
2018-10-07 13:57:53 +0300

    Importing Project

---
+++ HelloName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  Write a C# Sharp program to print Hello and your name in a separate line.
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Marios Daskalas!");
+            Console.ReadLine();
+        }
+    }
+}
